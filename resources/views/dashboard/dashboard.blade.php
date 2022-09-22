ed6a8a8e3a798df3cec97a1f9973429caf915858
Sayed Abu Zayed Hossain <zayedbd24@gmail.com>
2022-09-23 00:32:47 +0600

    project install and category table created

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('pagetitle')
+    Capital
+@endsection
+@section('content')
+<div class="container-fluid p-0">
+    <h1 class="h3 mb-3">Dashboard</h1>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title mb-0">dashboard</h5>
+                </div>
+                <div class="card-body">
+                    <p>Hello World</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
